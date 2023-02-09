9d1783e0d6ef39cec385944a9456f5b80ed23fe7
Mahmudul Hasan <m.hasan116114@gmail.com>
2023-02-10 04:26:48 +0600

    Cleaned up and Optimized functions

--- Grid.fs
+++ Grid.fs
@@ -1,11 +1,6 @@
 ﻿module Grid
 
-open System
-open Utils
-
-type GridItem =
-    | Character of char: char * color: Option<ConsoleColor>
-    | LineBreak
+open GridItem
 
 type Grid = {
     Row    : int
@@ -13,45 +8,17 @@ type Grid = {
     Items  : GridItem list
 }
 
-module GridItem =
-    let replicate (count: int) (item: GridItem) : list<GridItem> =
-        fun _ -> item
-        |> List.init count
-
-    let replicateList (count: int) (li: list<GridItem>) : list<GridItem> =
-        [1 .. count]
-        |> List.fold (fun grid _ -> grid @ li) []
-
-    let print (item: GridItem) =
-        match item with
-        | LineBreak -> printfn ""
-        |Character (char, maybeColor) ->
-            char
-            |> sprintf "%c "
-            |> printColored (Option.defaultValue Console.ForegroundColor maybeColor)
-
-    let charToCharacter (color: option<ConsoleColor>) (c: char) : GridItem =
-        Character (c, color)
-
-
 let stringToGridRow (s: string) : list<GridItem> =
     s
     |> Seq.map (fun c -> GridItem.charToCharacter None c)
     |> List.ofSeq
     |> fun li -> li @ [LineBreak]
 
-let stringAllGreen (s: string) : list<GridItem> =
-    s
-    |> Seq.map (fun c -> GridItem.charToCharacter (Some ConsoleColor.Green) c)
-    |> List.ofSeq
-    |> fun li -> li @ [LineBreak]
-
-
 let generateInitialGrid (row: int) (column: int): Grid =
     {   
-        Row = row
+        Row    = row
         Column = column
-        Items =
+        Items  =
             '*'
             |> GridItem.charToCharacter None
             |> GridItem.replicate column
